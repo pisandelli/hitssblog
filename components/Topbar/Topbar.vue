260d808f3b6cd333ff4c645b5c6787fdc3751d91
Pedro Pisandelli <pisandelli@gmail.com>
2024-02-27 18:42:09 -0300

    chore(layout): add default layout

---
+++ components/Topbar/Topbar.vue
@@ -0,0 +1,22 @@
+<script lang='ts' setup>
+/**
+* Topbar component
+* @name 'Topbar'
+* @version 1.0.0
+*/
+</script>
+
+<template lang="pug">
+.topbar
+  ClusterL(between)
+    span Logo
+    span user
+
+</template>
+
+<style lang="stylus" scoped>
+.topbar
+  padding: calc(var(--gap) / 2) var(--gap)
+  background-color: var(--color-primary)
+  color: var(--color-neutral)
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<script lang='ts' setup>
+/**
+* Default layout Header-Content-Footer.
+* @name 'DefaultLayout'
+* @version 1.0.0
+*/
+</script>
+
+<template lang="pug">
+BasicLayout.layout
+  header
+    Topbar
+  main
+    slot
+  footer
+    p FOOTER
+</template>
+<style lang='stylus' scoped>
+.layout
+  gap: var(--gap) 0
+</style>
